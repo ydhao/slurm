77a68ef68bf006c8bebf6516ef19f595109cc404
Brian Christiansen <brian@schedmd.com>
2018-05-31 22:05:14 -0600

    Retry on ECONNRESET - Connection reset by peer

--- src/slurmd/slurmstepd/mgr.c
+++ src/slurmd/slurmstepd/mgr.c
@@ -238,7 +238,8 @@ _send_srun_resp_msg(slurm_msg_t *resp_msg, uint32_t nnodes)
 		if (rc == SLURM_SUCCESS)
 			break;
 
-		if ((errno != ETIMEDOUT) && (errno != ENOTCONN)) {
+		if ((errno != ETIMEDOUT) && (errno != ENOTCONN) &&
+		    (errno != ECONNRESET)) {
 			error("%s: failed to send srun resp msg rc:%d (%m)",
 			      __func__, rc);
 			break;
