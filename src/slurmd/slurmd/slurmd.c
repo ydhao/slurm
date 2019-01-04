ea00b0d48f50df42e9b05803f843742c65eecc8d
Tim Wickberg <tim@schedmd.com>
2019-01-03 21:19:00 -0700

    WIP

--- src/slurmd/slurmd/slurmd.c
+++ src/slurmd/slurmd/slurmd.c
@@ -2174,6 +2174,9 @@ static bool _is_core_spec_cray(void)
  */
 static int _core_spec_init(void)
 {
+#if defined(__APPLE__)
+	fatal("%s: not supported on macOS", __func__);
+#else
 	int i, rval;
 	pid_t pid;
 	uint32_t task_params;
@@ -2293,6 +2296,7 @@ static int _core_spec_init(void)
 	_resource_spec_fini();
 
 	return SLURM_SUCCESS;
+#endif
 }
 
 /*
