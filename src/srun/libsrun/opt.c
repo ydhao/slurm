bad4ad88cbed73e8e8444372c7a8b2047db47e03
Tim Wickberg <tim@schedmd.com>
2019-03-18 12:11:58 -0600

    Convert --verbose option parsing to new form.

--- src/srun/libsrun/opt.c
+++ src/srun/libsrun/opt.c
@@ -111,7 +111,6 @@ int	pass_number = 0;
 time_t	srun_begin_time = 0;
 bool	tres_bind_err_log = true;
 bool	tres_freq_err_log = true;
-int	_verbose = 0;
 
 /*---- forward declarations of static variables and functions  ----*/
 typedef struct env_vars env_vars_t;
@@ -130,7 +129,6 @@ struct option long_options[] = {
 	{"relative",         required_argument, 0, 'r'},
 	{"threads",          required_argument, 0, 'T'},
 	{"unbuffered",       no_argument,       0, 'u'},
-	{"verbose",          no_argument,       0, 'v'},
 	{"version",          no_argument,       0, 'V'},
 	{"wait",             required_argument, 0, 'W'},
 	{"disable-status",   no_argument,       0, 'X'},
@@ -463,7 +461,7 @@ extern int initialize_and_process_args(int argc, char **argv, int *argc_off)
 		if (!_opt_verify())
 			exit(error_exit);
 
-		if (_verbose)
+		if (opt.verbose)
 			slurm_print_set_options(&opt);
 
 		if (spank_init_post_opt() < 0) {
@@ -558,7 +556,6 @@ static void _opt_default(void)
 		opt.uid			= uid;
 		sropt.unbuffered	= false;
 		sropt.user_managed_io	= false;
-		_verbose		= 0;
 	}
 
 	/*
@@ -947,7 +944,6 @@ static bitstr_t *_get_pack_group(const int argc, char **argv,
 static void _set_options(const int argc, char **argv)
 {
 	int opt_char, option_index = 0, max_val = 0, tmp_int;
-	int  max_verbose = 0;
 	struct utsname name;
 	bool ntasks_set_opt = false;
 	bool nodes_set_opt = false;
@@ -1080,9 +1076,6 @@ static void _set_options(const int argc, char **argv)
 			break;
 		case (int)'u':
 			sropt.unbuffered = true;
-			break;
-		case (int)'v':
-			max_verbose++;
 			break;
 		case (int)'V':
 			print_slurm_version();
@@ -1369,8 +1362,6 @@ static void _set_options(const int argc, char **argv)
 		}
 	}
 
-	_verbose = MAX(_verbose, max_verbose);
-
 	/* This means --ntasks was read from the environment.  We will override
 	 * it with what the user specified in the hostlist. POE launched
 	 * jobs excluded (they have the SLURM_STARTED_STEP env var set). */
@@ -1535,7 +1526,7 @@ static bool _opt_verify(void)
 		     sropt.slurmd_debug);
 	}
 
-	if (opt.quiet && _verbose) {
+	if (opt.quiet && opt.verbose) {
 		error ("don't specify both --verbose (-v) and --quiet (-Q)");
 		verified = false;
 	}
